8b80a8972aaa2502dfff24f03b0e981ece1fd868
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-08-22 23:45:08 -0500

    Se capturó un evento, en este caso un click en un botón, se obtuvieron datos de dos edit text y se validaron

--- app/src/main/java/com/seguidor/juanix/primeraapp/LayoutActivity.java
+++ app/src/main/java/com/seguidor/juanix/primeraapp/LayoutActivity.java
@@ -2,15 +2,43 @@ package com.seguidor.juanix.primeraapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Juanix on 22/08/2016.
  */
 public class LayoutActivity extends Activity {
 
+    private EditText userNameEditText;
+    private EditText passwordEditText;
+
+    private Button loginButton;
+
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
+        //Se pone el layout linear_layout o table_layout
         setContentView(R.layout.table_layout);
+
+        userNameEditText=(EditText) findViewById(R.id.editTextUsername);
+        passwordEditText=(EditText) findViewById(R.id.editTextPassword);
+
+        loginButton=(Button) findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userNameEditText.getText().toString().equals("admin") &&
+                        passwordEditText.getText().toString().equals("admin")){
+                    finish();
+                }
+                else{
+                    Log.d("Login", "Wrong username/password");
+                }
+            }
+        });
     }
 }
