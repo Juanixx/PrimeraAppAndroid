a3574b9b70ab2c47fc9f1f49292d9b2d18d38471
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-08-22 23:14:23 -0500

    Se utilizó Table Layout para conocer su funcionamiento

---
+++ app/src/main/java/com/seguidor/juanix/primeraapp/LayoutActivity.java
@@ -0,0 +1,16 @@
+package com.seguidor.juanix.primeraapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Juanix on 22/08/2016.
+ */
+public class LayoutActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.table_layout);
+    }
+}
