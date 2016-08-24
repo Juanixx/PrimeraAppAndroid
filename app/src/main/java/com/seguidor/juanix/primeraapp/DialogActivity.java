3ba65c544aaae536d6a8f26d1df8189fa737cc2b
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-08-23 23:42:46 -0500

    Se creó un diálogo en lista y otro con CheckBox

--- app/src/main/java/com/seguidor/juanix/primeraapp/DialogActivity.java
+++ app/src/main/java/com/seguidor/juanix/primeraapp/DialogActivity.java
@@ -5,18 +5,26 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Juanix on 23/08/2016.
  */
 public class DialogActivity extends Activity {
 
+    //Variable para el diálogo en lista
+    final CharSequence[] items= {"Blue","Red","Yellow"};
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        showDialog();
+        //Para mostrar un diálogo modal
+        //showDialog();
+        //showDialogWithList();
+        showDialogWithCheckBox();
     }
 
+    //Para crear un diálogo modal
     private void showDialog(){
         new AlertDialog.Builder(this)
                 .setTitle("Titulo")
@@ -33,5 +41,38 @@ public class DialogActivity extends Activity {
                         Log.d("Dialog","Seguimos en la aplicación");
                     }
                 }).show();
+    }
+
+    //Para crear un diálogo en lista
+    private void showDialogWithList(){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+
+        builder.setTitle("Pick a color");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),items[i],Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog alert =builder.create();
+        alert.show();
+    }
+
+    //Para crear un diálogo con CheckBox
+    //Para hacer que sea por CheckBox se añade setSingleChoiceItems y un -1 en seguida indicando item seleccionado por defecto
+    private void showDialogWithCheckBox(){
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+
+        builder.setTitle("Pick a color");
+        builder.setSingleChoiceItems(items, -1,new DialogInterface.OnClickListener() {//El -1 es para indicar el item señalado por defecto
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),items[i],Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog alert =builder.create();
+        alert.show();
     }
 }
