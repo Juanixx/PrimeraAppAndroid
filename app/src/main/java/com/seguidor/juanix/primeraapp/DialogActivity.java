fc7746ce7901bb207a72072108f99f7782bfb222
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-08-26 20:56:57 -0500

    Se creó una barra de progreso (diálogo progresivo), con fallas, no funciona bien del todo

--- app/src/main/java/com/seguidor/juanix/primeraapp/DialogActivity.java
+++ app/src/main/java/com/seguidor/juanix/primeraapp/DialogActivity.java
@@ -2,8 +2,10 @@ package com.seguidor.juanix.primeraapp;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,14 +16,24 @@ public class DialogActivity extends Activity {
 
     //Variable para el diálogo en lista
     final CharSequence[] items= {"Blue","Red","Yellow"};
+    private ProgressDialog barProgressDialog;
+
+    //Para crear handlers
+    //El handler permite manejar la post ejecución del handler
+    private Handler updateBarHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        showBarDialog();
         //Para mostrar un diálogo modal
         //showDialog();
         //showDialogWithList();
-        showDialogWithCheckBox();
+        //showDialogWithCheckBox();
+        //showRingDialog();
+
+
+
     }
 
     //Para crear un diálogo modal
@@ -74,5 +86,63 @@ public class DialogActivity extends Activity {
 
         AlertDialog alert =builder.create();
         alert.show();
+    }
+
+    //Para crear un diálogo de progreso, como una barra o un círculo cargándose
+
+    //En éste caso un círculo cargándose
+    private void showRingDialog(){
+        final ProgressDialog ringProgressDialog = ProgressDialog.show(DialogActivity.this, "Espere", "Descargando..", true);
+        ringProgressDialog.setCancelable(true);//Con esto se especifica si el dialogo es removible de la pantalla
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ringProgressDialog.dismiss();
+            }
+        }).start();
+    }
+
+    //En éste caso una barra cargándose
+    private void showBarDialog(){
+        barProgressDialog = new ProgressDialog(DialogActivity.this);
+
+        barProgressDialog.setTitle("Espere...");
+        barProgressDialog.setMessage("Descargando");
+        barProgressDialog.setProgressStyle(barProgressDialog.STYLE_HORIZONTAL);
+        barProgressDialog.setProgress(0);
+        barProgressDialog.setMax(20);
+        barProgressDialog.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                while (barProgressDialog.getProgress() <= barProgressDialog.getMax());//El progreso es menor o igual al máximo especificado anteriormente
+
+                    Thread.sleep(2000);//Cada dos segundos
+                    //Apartir de aquí se controla la post ejecución del hilo
+                    //Con ésto, se incrementará de dos en dos la barra hasta llegar al maximo, que es 20
+                    updateBarHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            barProgressDialog.incrementProgressBy(2);
+                        }
+                    });
+                    if (barProgressDialog.getProgress() == barProgressDialog.getMax()){
+                        barProgressDialog.dismiss();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
     }
 }
